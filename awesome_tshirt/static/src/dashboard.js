f8fc11409efa872bfec8d8521fc9ba9240765c1f
dmetree01 <mrblond.rivz@yandex.ru>
2024-10-21 14:03:51 +0300

    implement awesome_tshirt module

--- awesome_tshirt/static/src/dashboard.js
+++ awesome_tshirt/static/src/dashboard.js
@@ -1,11 +1,73 @@
 /** @odoo-module **/
 
-import { Component } from "@odoo/owl";
+import { Component, useSubEnv, onWillStart } from "@odoo/owl";
 import { registry } from "@web/core/registry";
+import { Layout } from "@web/search/layout";
+import { getDefaultConfig } from "@web/views/view";
+import { useService } from "@web/core/utils/hooks";
+import { Domain } from "@web/core/domain";
+import { Card } from "./card";
+import { PieChart } from "./pie_chart";
 
-class AwesomeDashboard extends Component {}
+class AwesomeDashboard extends Component {
+    setup() {
+        useSubEnv({
+            config: {
+                ...getDefaultConfig(),
+                ...this.env.config,
+            }
+        });
 
-AwesomeDashboard.components = {};
+        this.display = {
+            controlPanel: { "top-right": false, "bottom-right": false },
+        };
+
+        this.action = useService("action");
+
+        this.tshirtService = useService("tshirtService");
+        this.keyToString = {
+            average_quantity: "Average amount of t-shirt by order this month",
+            average_time: "Average time for an order to go from 'new' to 'sent' or 'cancelled'",
+            nb_cancelled_orders: "Number of cancelled orders this month",
+            nb_new_orders: "Number of new orders this month",
+            total_amount: "Total amount of new orders this month",
+        };
+        onWillStart(async () => {
+            this.statistics = await this.tshirtService.loadStatistics();
+        });
+    }
+
+    openCustomerView() {
+        this.action.doAction("base.action_partner_form");
+    }
+
+    openLast7DaysOrders() {
+        const domain =
+            "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d'))]";
+        this._openOrders("Last 7 days orders", domain);
+    }
+
+    openLast7DaysCancelledOrders() {
+        const domain =
+            "[('create_date','>=', (context_today() - datetime.timedelta(days=7)).strftime('%Y-%m-%d')), ('state','=', 'cancelled')]";
+        this._openOrders("Last 7 days cancelled orders", domain);
+    }
+
+    _openOrders(title, domain) {
+        this.action.doAction({
+            type: "ir.actions.act_window",
+            name: title,
+            res_model: "awesome_tshirt.order",
+            domain: new Domain(domain).toList(),
+            views: [
+                [false, "list"],
+                [false, "form"],
+            ],
+        });
+    }
+}
+
+AwesomeDashboard.components = { Layout, Card, PieChart };
 AwesomeDashboard.template = "awesome_tshirt.clientaction";
 
 registry.category("actions").add("awesome_tshirt.dashboard", AwesomeDashboard);
